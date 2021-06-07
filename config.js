f0444c6bc632c206b8868147c00c3d3c365049c2
joaoltf <62778071+joaoltf@users.noreply.github.com>
2021-06-07 12:00:45 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,47 @@
+// Username displayed in greeting.
+var userName = "João";
+
+// Default Searchengine
+// Available: Google DuckDuckGo Bing Yahoo
+var searchEngine = "Google";
+
+// Cards
+var cards = [
+	{
+		name: "Admin",
+		bookmarks: {
+			"DS218+": "https://192.168.1.218:5218/",
+			"Portainer": "http://192.168.1.218:9000/",
+			"OpenWRT": "http://192.168.1.1/",
+			"Speedtest": "http://192.168.1.218:8765/",
+			"FreshRSS": "http://192.168.1.218:32775/",
+		}
+	},
+	{
+		name: "Media",
+		bookmarks: {
+			"Radarr": "http://192.168.1.218:32783/",
+			"Sonarr": "http://192.168.1.218:32784/",
+			"Bazarr": "http://192.168.1.218:36547/series",
+			"Jellyfin": "http://192.168.1.218:32773/",
+			"Bibliogram": "http://192.168.1.218:10404/",
+		}
+	},
+	{
+		name: "Downloads",
+		bookmarks: {
+			"Jackett": "http://192.168.1.218:32782/UI/Dashboard",
+			"Hydra2": "http://192.168.1.218:32793/",
+			"NZBGet": "http://192.168.1.218:32792/",
+			"qBittorrent": "http://192.168.1.218:8080/",
+			"Prowlarr": "http://192.168.1.218:32774/"
+		}
+	},
+	{
+		name: "Pessoal",
+		bookmarks: {
+			"Shaarli": "http://192.168.1.218:32781/",
+			"WebRHV": "https://webrhv.min-saude.pt/",
+		}
+	}
+]
